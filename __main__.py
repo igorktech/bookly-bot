0794d192819f21429e0ffef1673ec22c96156cef
Igor <igoranchikkuzmin@yandex.ru>
2024-03-26 00:36:48 +0100

    minor(main): fix port issue

--- __main__.py
+++ __main__.py
@@ -1,10 +1,8 @@
+import os
+import threading
+from flask import Flask, request, jsonify
 from logger import configure_logger
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    MessageHandler,
-    filters,
-)
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from services.db_manager import db_client
 from engine.tools import Tools
 from engine.engine import Engine
@@ -22,23 +20,36 @@ MESSAGE_HANDLERS = {
     "message": handlers.message_handler,
 }
 
+# Initialize logger
 logger = configure_logger()
 
+# Check for necessary config vars
 if not config.TELEGRAM_BOT_TOKEN or not config.OPENAI_API_KEY:
     raise ValueError(
-        "TELEGRAM_BOT_TOKEN and OPENAI_API_KET env variables"
-        "wasn't implemented in .env (both should be initialized)."
-    )
+        "TELEGRAM_BOT_TOKEN and OPENAI_API_KEY env variables weren't implemented in .env (both should be initialized).")
+
+# Initialize Flask app
+app = Flask(__name__)
 
 
-def main():
-    app = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
+@app.route('/')
+def index():
+    return jsonify({"status": "ok"})
+
+
+def run_flask_app():
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
+def run_telegram_bot():
+    application = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
 
     tools = Tools(config.OPENAI_API_KEY)  # Initialize Tools with the OpenAI API key
     appointment_manager = AppointmentManager()
-    app.bot_data["engine"] = Engine(tools.builder, appointment_manager)
+    application.bot_data["engine"] = Engine(tools.builder, appointment_manager)
 
-    app.add_handlers(
+    application.add_handlers(
         [
             CommandHandler("start", COMMAND_HANDLERS["start"]),
             CommandHandler("help", COMMAND_HANDLERS["help"]),
@@ -47,15 +58,16 @@ def main():
         ]
     )
 
-    app.run_polling()
+    application.run_polling()
 
 
 if __name__ == "__main__":
     try:
-        main()
-    except Exception:
-        import traceback
-
+        # Run Flask app in a separate thread
+        threading.Thread(target=run_flask_app).start()
+        # Run the main function for the Telegram bot
+        run_telegram_bot()
+    except Exception as e:
         logger.warning(traceback.format_exc())
     finally:
         db_client.close_db()
